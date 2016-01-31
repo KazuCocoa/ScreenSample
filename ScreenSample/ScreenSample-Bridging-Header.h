d4bfbf707f7f222c8be34c9715315a9e395b16ad
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-02-01 00:36:30 +0900

    import DHSmartScreenshot

---
+++ ScreenSample/ScreenSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ScreenSample-Bridging-Header.h
+//  ScreenSample
+//
+//  Created by 松尾和昭 on 31/01/2016.
+//  Copyright © 2016 kazucocoa. All rights reserved.
+//
+
+#ifndef ScreenSample_Bridging_Header_h
+#define ScreenSample_Bridging_Header_h
+
+#import "DHSmartScreenshot/DHSmartScreenshot.h"
+
+#endif /* ScreenSample_Bridging_Header_h */
